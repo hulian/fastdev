dc6c552e5563fde4aa80eee3d54c8c466344259d
yami <yami@DESKTOP-0NH8G20>
2016-11-30 14:57:43 +0800

    777

--- fastdev-balancer/src/test/java/com/auth/test/dao/PartitionInfoDaoTest.java
+++ fastdev-balancer/src/test/java/com/auth/test/dao/PartitionInfoDaoTest.java
@@ -17,7 +17,7 @@ public class PartitionInfoDaoTest {
 
 	@Test
 	public void testSave() {
-		//7788899
+		//7788899777
 		transactionManager.autoConnection(()->{
 			transactionManager.doInTransaction(() -> {
 
